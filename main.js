e010423241787fd3d072883fdcf3e69606ef4bb0
Misael Guerrero <mguerrerowest@gmail.com>
2022-11-28 12:55:25 -0500

    Add clear function

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
-let operationValue = ''
-let resultValue = ''
-let operator = ''
+let operationValue1 = '';
+let operationValue2 = '';
+let resultValue = '';
+let operator = '';
 
 const operationScreen = document.querySelector('.top');
 const resultScreen = document.querySelector('.bottom');
 
 const numberButtons = document.querySelectorAll('.digit');
-
 const operatorButtons = document.querySelectorAll('.operator')
 
 const clearButton = document.querySelector('.clear');
 const decimalButton = document.querySelector('.decimal');
-const equalsBUtton = document.querySelector('.equals');
+const equalsButton = document.querySelector('.equals');
 
 numberButtons.forEach((number) => number.addEventListener('click', function(e){  //For each button we add an event listener,
-    displayNum(e.target.textContent);                                            //then we target the text within each button (1, 2, 3...)
-    operationScreen.textContent = operationValue;                                //the text inside operation screen is updated by matching it to 
+    displayNum1(e.target.textContent);                                            //then we target the text within each button (1, 2, 3...)
+    operationScreen.textContent = operationValue1;                                //the text inside operation screen is updated by matching it to 
 }))                                                                              //operationValue variable, which is being updated by the displayNum function below.
 
 
+operatorButtons.forEach((operator) => operator.addEventListener('click', function(e){
+    displayOperator(e.target.textContent);     
+    operationScreen.textContent = operator; 
+    resultScreen.textContent = operationValue2;
+}))                                            
 
-function displayNum(num) {
-    if (operationValue.length <= 15) {
-        operationValue += num; //the number on the operation screen is updated with the num passed into this function
-    }
+
+function displayNum1(num) {
+    if (operationValue1.length <= 10) {
+        operationValue1 += num; //the number on the operation screen is 
+                               //updated with the num passed into this function
+    }}
+
+function displayOperator(oper){
+    operator = oper;
+    operationValue2 = operationValue1;
+    operationValue1 = '';
 }
+
+clearButton.addEventListener('click', function(){
+    operationValue1 = '';
+    resultValue = '';
+    operator = '';
+    operationScreen.textContent = '';
+    resultScreen.textContent = '';
+})
